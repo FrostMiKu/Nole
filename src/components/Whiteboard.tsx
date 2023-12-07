ce00fb46e3fcd3bb7187e1c0b17400ad0020b538
FrostMiKu <i@frostmiku.com>
2023-12-08 03:11:36 +0800

    update whiteboard

--- src/components/Whiteboard.tsx
+++ src/components/Whiteboard.tsx
@@ -5,65 +5,63 @@ import {
   WelcomeScreen,
   exportToBlob,
   loadFromBlob,
-  getSceneVersion
+  getSceneVersion,
 } from "@excalidraw/excalidraw";
-import { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types/types";
-import { useAtom } from "jotai";
-import { useEffect, useState } from "react";
+import {
+  ExcalidrawInitialDataState,
+} from "@excalidraw/excalidraw/types/types";
+import { useAtomValue } from "jotai";
+import { useCallback, useEffect, useState } from "react";
 import OnceInputer from "./OnceInputer";
 import path from "path-browserify";
+import { debounce } from "../lib/utils";
 
 function Whiteboard() {
-  const [currentFile, setCurrenFile] = useAtom(CurrentFileAtom);
+  const [ sceneVersion, setSceneVersion ] = useState<number>(0);
+  const [ initialData, setInitialData ] = useState<ExcalidrawInitialDataState>();
+  const currentFile = useAtomValue(CurrentFileAtom);
   const [renameing, setRenameing] = useState<boolean>(false);
-  const [excalidrawAPI, setExcalidrawAPI] =
-    useState<ExcalidrawImperativeAPI | null>(null);
 
   useEffect(() => {
-    if (!currentFile || !excalidrawAPI) return;
-    const disposer = excalidrawAPI.onPointerUp(() => {
-      const elements = excalidrawAPI.getSceneElements();
-      const appState = excalidrawAPI.getAppState();
-      const files = excalidrawAPI.getFiles();
-      const saver = currentFile.clone();
-      exportToBlob({
+    currentFile?.readAsBinary().then((buffer) => {
+      const blob = new Blob([buffer], { type: "image/png" });
+      loadFromBlob(blob, null, null).then((data) => {
+        setInitialData(data);
+        setSceneVersion(getSceneVersion(data.elements));
+      }).catch(()=>{
+        window.nole.notify.error({
+          content: "File load failed! Is it a valid Excalidraw file?",
+        });
+      });
+    });
+  },[]);
+
+
+
+  const autosave = useCallback(
+    debounce(async (elements, appState, files) => {
+      if (!currentFile) return;
+      const blob = await exportToBlob({
         appState: {
           ...appState,
           exportEmbedScene: true,
+          exportBackground: false,
+          exportScale: 2,
         },
         elements,
         files,
         mimeType: "image/png",
-      }).then((blob) => {
-        blob.arrayBuffer().then((buffer) => {
-          saver.writeAsBinary(buffer);
-        });
       });
-    });
-    currentFile.readAsBinary().then((buffer) => {
-      if (buffer.length === 0) {
-        excalidrawAPI.resetScene();
-        return;
-      }
-      const blob = new Blob([buffer], { type: "image/png" });
-      loadFromBlob(blob, null, null)
-        .then((data) => {
-          excalidrawAPI.updateScene(data);
-        })
-        .catch((_) => {
-          window.nole.notify.error({
-            content: "File load failed! Is it a valid Excalidraw file?",
-          });
-          setCurrenFile(null);
-        });
-    });
-    return () => {
-      disposer();
-    };
-  }, [currentFile, excalidrawAPI]);
+      const buffer = await blob.arrayBuffer();
+      await currentFile.writeAsBinary(buffer);
+    }, window.nole.config.autosave_delay),
+    []
+  );
 
+  if (!initialData) return <div className="w-full h-full bg-slate-200 animate-pulse" />;
   return (
     <Excalidraw
+      initialData={initialData}
       UIOptions={{
         canvasActions: {
           loadScene: false,
@@ -74,10 +72,11 @@ function Whiteboard() {
           toggleTheme: true,
         },
       }}
-      excalidrawAPI={(api) => setExcalidrawAPI(api)}
-      onChange={(elements, appState, files)=>{
-        console.log(getSceneVersion(elements));
-        
+      onChange={(elements, appState, files) => {
+        const currentVersion = getSceneVersion(elements);
+        if(currentVersion <= sceneVersion) return;
+        setSceneVersion(currentVersion);
+        autosave(elements, appState, files);
       }}
     >
       <WelcomeScreen>
@@ -107,17 +106,19 @@ function Whiteboard() {
                     currentFile!.parent,
                     newFilename + currentFile!.extname
                   )
-                ).catch((e) => {
+                )
+                .catch((e) => {
                   window.nole!.notify.error({ content: e as string });
                 })
                 .finally(() => setRenameing(false));
             }}
           />
         ) : (
-          <div className="h-10 overflow-hidden w-full flex flex-row-reverse items-center font-bold text-lg"
-          onDoubleClick={()=>{
-            setRenameing(true);
-          }}
+          <div
+            className="h-10 overflow-hidden w-full flex flex-row-reverse items-center font-bold text-lg"
+            onDoubleClick={() => {
+              setRenameing(true);
+            }}
           >
             {currentFile?.name}
           </div>
